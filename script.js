8af6d444630cf7692bb73338da4da5d98f935335
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-07-14 21:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0) {
+    return 0;
+  } else if (num === 1 || num === 2) {
+    return 1;
+  } else {
+    let prevPrev = 0;
+    let prev = 1;
+    let current = 1;
+
+    for (let i = 3; i <= num; i++) {
+      prevPrev = prev;
+      prev = current;
+      current = prevPrev + prev;
+    }
+
+    return current;
+  }
 }
 
 module.exports = fibonacci;
